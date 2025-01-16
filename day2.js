ef63c72abcd4e73950b3a3de2ba2ac2c0ecac9f7
vyankatesh gupta <vyankateshg17@gmail.com>
2025-01-16 20:46:06 +0530

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,25 @@
+// you need to download node.js for using js file in console (cmd)
+// we will see variables and data type of js in day 2 
+
+a = 12
+console.log(a)
+b = 12.34
+c = a+b;
+bc = "radhe radhe ";
+
+let name = "vyankatesh"; //can change the values of it 
+var city = "ahmedabad"; // character variables 
+let age = 19; // we can change it 
+const farther_name = "vishnu gupta"// can not be changed 
+
+console.log("name : ", name);
+console.log("age  : ", age);
+console.log("City : ", city);
+console.log("farther's name : ", farther_name);
+
+// other data type of js 
+let isStudent = true;// boolean
+let score =  8.9; // number 
+let hoppe = ["coding", "music","boxing","others"]//array
+
+//we will see other data type in day 3 thank you 
