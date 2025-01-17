b500a65a8fa2f00f3772eae61a313a447e97e43c
vyankatesh gupta <vyankateshg17@gmail.com>
2025-01-17 23:09:56 +0530

    Create day3_operators.js

---
+++ day3_operators.js
@@ -0,0 +1,26 @@
+let x = 10;
+let y = 20;
+
+//arithmetic operators
+console.log("addition : x + y = ",x+y);
+console.log("subtraction : x - y = ",x-y);
+console.log("multipliction : x * y = ",x*y);
+console.log("division: x / y = ",x/y);
+console.log("modulus : x % y = ",x%y);
+
+//comparison operators 
+console.log("x > y = ",x>y);
+console.log("x < y = ",x<y);
+console.log("x == y  ",x==y);
+console.log("x === y  ",x===y); //stric check
+console.log("x != y = ",x!=y);
+console.log("x !== y = ",x!==y);
+
+//logical operators
+//AND OR NOT 
+let x = true, y = false;
+console.log("AND : x && y = ",x*y);
+console.log("OR: x || y = ",x/y);
+console.log("NOT : x ! y = ",x%y);
+
+//in day 4we  will see string datatype and it's functions 
