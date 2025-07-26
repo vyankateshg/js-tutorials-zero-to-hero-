96fa32b4522ec0d5bc98e98b8a4103ca37fd1ed3
vyankatesh gupta <vyankateshg17@gmail.com>
2025-07-26 20:44:01 +0530

    Create classesObjectsExample.js

---
+++ classesObjectsExample.js
@@ -0,0 +1,30 @@
+// let DATA = "secret information";
+
+// class User{
+//     constructor(name,email){
+//         this.name = name ;
+//         this.email = email;
+//     }
+
+//     viewData(){
+//         console.log(`the name is ${this.name} and email ${this.email}`)
+//         console.log("data = ",DATA)
+//     }
+// }
+
+
+// class Admin extends User{
+//     constructor(name ,email){
+//         super(name,email);
+//     }
+//     editData(){
+//         DATA = "some new value"
+//     }
+// }
+// let student1 =new User("vyankatesh","vyankateshg17@gmial.com");
+
+// student1.viewData();
+
+// let teacher1 = new User("Dean","dean@uni.com")
+
+// let admin1 = new Admin("admin","admin@jg.com"); 
